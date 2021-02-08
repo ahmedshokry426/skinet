a936dacda8486002e8cc1722f8a0096483a94b4b
“ahmedshokry426” <ahmed.shokrg@gmail.com>
2021-02-08 19:38:27 +0200

    End of section 6

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -17,6 +17,21 @@ namespace Infrastructure.Data
                 query = query.Where(spec.Criteria); // p=>p.ProdcutTypeId == id    
             }
 
+            if(spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if(spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if(spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
             /*return await _context.Products
                 .Include(p => p.ProductType)
                 .Include(p => p.ProductBrand)
